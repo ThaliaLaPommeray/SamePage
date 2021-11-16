1507b758e9619e12c378e829ce4ccc58c8d8d2ad
deBuggy98 <kinikxluna@gmail.com>
2021-11-15 22:49:19 -0500

    Spring Framework Installation

---
+++ SamePage/src/test/java/com/Group11/SamePage/SamePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Group11.SamePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
