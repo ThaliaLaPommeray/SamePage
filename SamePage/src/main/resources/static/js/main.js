2060917b00daa3d0a399626cc418f53a8b217d72
isaactuckey <isaactuckey@knights.ucf.edu>
2021-11-27 21:41:08 -0500

    extra ajax

--- SamePage/src/main/resources/static/js/main.js
+++ SamePage/src/main/resources/static/js/main.js
@@ -2,3 +2,27 @@ function logout() {
     sessionStorage.clear();
     window.location = "loginpage";
 }
+
+function openSignUp(){
+    window.location = "signuppage";
+}
+
+function openLogin(){
+    window.location = "loginpage";
+}
+
+function openBookEditor(){
+    window.location = "bookeditorpage";
+}
+
+function openBookView(){
+    window.location = "bookviewpage";
+}
+
+function openMainPage() {
+    window.location = "mainpage";
+}
+
+function openSubmissionEditor() {
+    window.location = "submissioneditor"
+}
